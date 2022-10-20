b77c055417d7119251527c9b98cff24345c56613
mohamed-elbermawy <mohamed77bermawy@gmail.com>
2022-10-20 16:46:35 +0200

    In And Out

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,101 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> expensesList = _context.Expenses;
+            return View(expensesList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(expense);
+            }
+        }
+
+        public IActionResult Update(int? Id)
+        {
+            var obj = _context.Expenses.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Update(expense);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(expense);
+            }
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            var obj = _context.Expenses.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            var obj = _context.Expenses.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Expenses.Remove(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
+
---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemsList = _context.Items;
+            return View(itemsList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
